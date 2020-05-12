8b8dc080c7d4982554ad28e5619bd5ad0fc512a8
Nikita <shriramnikita@gmail.com>
2020-05-12 19:46:06 +0530

    Added: functions to check row column and diagonal conditions

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -40,30 +40,69 @@ function displayBoard(){
 	echo "+---+---+---+"
 }
 
-function validateCell()
-{
+function validateCell(){
 	displayBoard
 	count=0
-	read -p "enter a cell position between 1 to 9 " position
-	for (( counter=1;counter<=9;counter++ ))
+	read -p "Enter cell position between 1 to 9:- " position
+	ticTacToe[$(($position))]="X"
+	count=$(($count+1))
+}
+
+function rowCheckforWinning(){
+	for (( i=1; i<=9; i=$((i+3)) ))
 	do
-		if [ $position -eq $counter ]
+		if [[ ${ticTacToe[$i]} -eq ${ticTacToe[(($i+1))]} && ${ticTacToe[$i]} -eq ${ticTacToe[(($i+2))]} ]]
 		then
-			count=1
-			break
+			row=0
+			break;
 		else
-			count=0
+			row=1
 		fi
 	done
-
-	if [ $count -eq 1 ]
-	then
-	 	ticTacToe[$i]=X
-		displayBoard  ${ticTacToe[$i]} 
-
-	else
-		validateCell
-
-	fi
+		echo $row
 }
-validateCell
+
+function columnCheckforWinning()
+{
+	column=0
+	for (( i=1; i<4; i++ ))
+	do
+		if [[ ${ticTacToe[$i]} -eq ${ticTacToe[(($i+3))]} && ${ticTacToe[$i]} -eq ${ticTacToe[(($i+6))]} ]]
+		then
+				column=0
+				break;
+		else
+				column=1
+		fi
+	done
+	echo $column
+}
+
+function diagonalCheckforWinning()
+{
+	if [[ ${ticTacToe[1]} -eq ${ticTacToe[5]} && ${ticTacToe[1]} -eq ${ticTacToe[9]} || ${ticTacToe[3]} -eq ${ticTacToe[5]} && ${ticTacToe[3]} -eq ${ticTacToe[7]} ]]
+	then
+			diagonal=0
+	else
+			diagonal=1
+	fi
+	echo $diagonal
+}
+
+function winner(){
+	toss
+	count=0
+	while [ $count -ne 9 ]
+	do
+		validateCell
+		row=$(rowCheckforWinning)
+		column=$(columnCheckforWinning)
+		diagonal=$(diagonalCheckforWinning)
+		if [[ $row -eq 0 || $column -eq 0 || $diagonal -eq 0 ]]
+		then
+			echo "You Win"
+			break
+		fi
+	done
+}
+winner
