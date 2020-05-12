257ddb7900a7d068ab51ef7ae46aa9100c38f1f3
Nikita <shriramnikita@gmail.com>
2020-05-12 21:08:25 +0530

    Added: computer plays its move

--- TicTacToe.sh
+++ TicTacToe.sh
@@ -2,6 +2,18 @@
 
 echo "Welcome to Tic Tac Toe Game"
 
+
+PLAYER_1="Human"
+PLAYER_2="Computer"
+
+positions=(- - - - - - - - -)  # initial positions
+
+player_one=true  # player switch init
+gamefinished=false  # is the game finished
+stall=false  # stall - if an invalid or empty move was input
+
+
+
 #uc1 function for reset board
 function resetBoard(){
 	for (( i=1; i<=9; i++ ))
@@ -40,12 +52,58 @@ function displayBoard(){
 	echo "+---+---+---+"
 }
 
-function validateCell(){
-	displayBoard
-	count=0
-	read -p "Enter cell position between 1 to 9:- " position
-	ticTacToe[$(($position))]="X"
-	count=$(($count+1))
+function readMove(){
+
+	 positions_str=$(printf "%s" "${positions[@]}")
+
+  	test_position_str $positions_str  # finish the game if all postiions have been taken or a player has won
+
+  if [ "$gamefinished" = false ] ; then
+
+    if [ "$stall" = false ] ; then
+      if [ "$player_one" = true ] ; then
+        prompt="Your move, "$player_1_str"?"
+      fi
+    else
+      stall=false
+    fi
+
+    if [ "$player_one" = true ] ; then
+      echo -e $prompt
+      read -d'' -s -n1 input  # read input
+
+      index=10  # init with nonexistent
+      case $input in
+            q) index=0;;
+            a) index=3;;
+            z) index=6;;
+            w) index=1;;
+            s) index=4;;
+            x) index=7;;
+            e) index=2;;
+            d) index=5;;
+            c) index=8;;
+      esac
+
+      if [ "${positions["$index"]}" == "-" ]; then
+        positions["$index"]="X"
+        player_one=false
+      else
+     stall=true  # prevent player switch
+      fi
+
+    else
+      # computer, choose your position!
+      set_next_avail_pos_index "O"
+      player_one=true
+    fi
+
+    init_game  # reinit, because positions persist
+  fi
+
+
+
+
 }
 
 function rowCheckforWinning(){
@@ -54,7 +112,6 @@ function rowCheckforWinning(){
 		if [[ ${ticTacToe[$i]} -eq ${ticTacToe[(($i+1))]} && ${ticTacToe[$i]} -eq ${ticTacToe[(($i+2))]} ]]
 		then
 			row=0
-			break;
 		else
 			row=1
 		fi
@@ -89,20 +146,53 @@ function diagonalCheckforWinning()
 	echo $diagonal
 }
 
-function winner(){
-	toss
-	count=0
-	while [ $count -ne 9 ]
-	do
-		validateCell
-		row=$(rowCheckforWinning)
-		column=$(columnCheckforWinning)
-		diagonal=$(diagonalCheckforWinning)
-		if [[ $row -eq 0 || $column -eq 0 || $diagonal -eq 0 ]]
-		then
-			echo "You Win"
-			break
-		fi
-	done
+
+function init_game 
+{
+  displayBoard
+  readMove
 }
-winner
+
+function end_game {
+  gamefinished=true
+  displayBoard
+}
+
+
+
+function testPosition
+ {
+  rows=$(rowCheckforWinning)
+  cols=$(columnCheckforWinning)
+  diagonals=$(diagonalCheckforWinning)
+
+  if [[ $rows =~ [X]{3,} || $cols =~ [X]{3,} || $diagonals =~ [X]{3,} ]]; then
+    end_game
+    printf $Player_1" wins! \n"
+    return
+  fi
+  if [[ $rows =~ [O]{3,} || $cols =~ [O]{3,} || $diagonals =~ [O]{3,} ]]; then
+    end_game
+   printf $PLAYER_2" wins! \n"
+    return
+  fi
+  if [[ ! $positions_str =~ [-] ]]; then
+    end_game
+    printf "Ends with a draw"$reset"\n"
+  fi
+}
+
+# get next available position and set it to value of argument
+function set_next_avail_pos_index {
+  available=()
+  for (( i = 0; i < ${#positions[@]}; i++ )); do
+    if [[ ${positions[$i]} == '-' ]]; then
+      available+=($i)
+    fi
+  done
+
+  rand=$(jot -r 1 0 $(( ${#available[@]}-1 )))  # random in range 0 to available_len
+  positions[${available[$rand]}]=$1
+}
+
+init_game
